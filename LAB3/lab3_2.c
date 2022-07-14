1e1c47f7647e7aa1884ed2b9395963bbd9b83b3b
WhileBELL <108038620+WhileBELL@users.noreply.github.com>
2022-07-14 17:48:30 +0700

    AddLab3_1,Lab3_2

---
+++ LAB3/lab3_2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() {
+    int i = 0 ; 
+    int N = 7 ;
+    printf( "--| Show 'Hello World' to Screen %d line(s) \n", N ) ;
+    for( i = 0 ; i < N ; i++ ) {
+        printf( "%d Hello World \n", i ) ;
+    }
+    char star = '*' ;
+    int j = 0 ; N = 8 ; i = 0 ;
+    printf( "\n" ) ;
+    while( i < N ) {
+        j = 0 ;
+        while( j < N ) {
+            printf( "%c", star ) ;
+            j++ ;
+        }
+        printf( "%s" , "\n" ) ;
+        i += 1 ;
+    }
+    return 0 ;
+}
